4f82e3377513fc687e1541cf4af625457e7aecbb
VIVITEK <baiborodovx@gmail.com>
2024-08-11 12:43:13 +0500

    last commit

--- gulp/tasks/svgSpriteFunc.js
+++ gulp/tasks/svgSpriteFunc.js
@@ -2,6 +2,12 @@ import svgSprite from "gulp-svg-sprite";
 
 export const svgSpriteFunc = () => {
   return app.gulp.src(app.path.src.svgicons, {})
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "SVG",
+        message: "Error <%= error.message %>"
+      })
+    ))
     .pipe(svgSprite({
       mode: {
         stack: {
